68fb7634c4426f2a018fb4bffd073b467e847499
wuyao19952016 <2438626764@qq.com>
2020-02-18 19:14:12 +0800

    修改1037

--- 1037_计算2的冥.cpp
+++ 1037_计算2的冥.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
-	int n,s=1;
-	cin>>n; 
-	for(int i=1;i<=n;i++)
-	{
-		s*=2;
-	}
-	cout<<s;
+//  方法1：循环累成 
+//	int n,s=1;
+//	cin>>n; 
+//	for(int i=1;i<=n;i++)
+//	{
+//		s*=2;
+//	}
+//	cout<<s;
+	int n;
+	cin>>n;
+	printf("%d",pow(2,n));
+	
 	return 0;
 }
