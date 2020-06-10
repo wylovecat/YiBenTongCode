080234230e0053238e30ca9dd48c9a82bc14bb21
wyloving <wuyaolovecat@qq.com>
2020-06-10 10:14:06 +0800

    修改1148源码

--- 05数组/1148_连续出现的字符.cpp
+++ 05数组/1148_连续出现的字符.cpp
@@ -1,37 +1,27 @@
 #include <iostream>
 #include <cstring>
-using namespace std; 
-
-int main(int argc, char** argv) {
-	int k;
+using namespace std;
+int main(){
 	char str[2505]={0};
-	int cnt=1;
-	bool flag=true;
+	int cnt=1,k,flag=1;
 	cin>>k;
 	cin>>str;
-	for(int i=1;str[i]!=0;i++){
-		if(str[i]==str[i-1]){
+	int len=strlen(str);
+	for(int i=0;i<len;i++){
+		if(str[i]==str[i+1]){
 			cnt++;
-		}
-		if(str[i]!=str[i-1]||str[i+1]=='\0')
-		{
+		}else{
 			if(cnt>=k){
-				flag=false;
-				cout<<str[i-1];
+				flag=0;
+				cout<<str[i];
 				break;
 			}
-			cnt=1;		
+			cnt=1;
 		}
-	}
-	if(k==1){
-		flag=false;
-		cout<<str[0];
 	}
 	if(flag){
 		cout<<"No";
 	}
-	
-
 	
 	
 	return 0;
